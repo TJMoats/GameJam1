43ae4c28430aba78ef32f236d3d92b39bcecdd19
Jaron Gin <jaronomasia@gmail.com>
2018-12-29 12:12:48 -0600

    TownProgress

---
+++ GameJam/Assets/Scripts/Scripts/AlphaSortedBrush.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityEditor
+{
+    [CustomGridBrush(true, true, true, "Alpha Sorted Brush")]
+    public class AlphaSortedBrush : GridBrush
+    {
+    }
+
+    [CustomEditor(typeof(AlphaSortedBrush))]
+    public class AlphaSortedBrushEditor : GridBrushEditor
+    {
+        public override GameObject[] validTargets
+        {
+            get
+            {
+                GameObject[] vt = base.validTargets;
+                Array.Sort(vt, (go1, go2) => string.Compare(go1.name, go2.name));
+                return vt;
+            }
+        }
+    }
+}
