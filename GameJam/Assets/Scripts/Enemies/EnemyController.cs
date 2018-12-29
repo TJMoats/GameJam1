cb8af830930e85061daf0c5d3961b4040b881f25
TJ Moats <tj@moats.io>
2018-12-29 00:45:18 -0700

    updating enemy controller

--- GameJam/Assets/Scripts/Enemies/EnemyController.cs
+++ GameJam/Assets/Scripts/Enemies/EnemyController.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using Sirenix.OdinInspector;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AIPath), typeof(Seeker))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Transform target;
+
+    private AIPath aiPath;
+    protected AIPath AIPath
     {
-        
+
+        get
+        {
+            if (aiPath == null)
+            {
+                aiPath = GetComponent<AIPath>();
+            }
+            return aiPath;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    [Button]
+    public void UpdateTarget()
+    {
+        AIPath.destination = target.position;
+
     }
 }
