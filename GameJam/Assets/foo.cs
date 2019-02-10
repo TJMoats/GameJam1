f76d9ad7c63e03b04ba6b25ce614ff230b1f3c9e
TJ Moats <tj@moats.io>
2019-02-09 23:44:24 -0700

    I'm about to do a mad dumb

---
+++ GameJam/Assets/foo.cs
@@ -0,0 +1,20 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foo : MonoBehaviour
+{
+    [Button]
+    public void FixMe()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Vector3 v = transform.GetChild(i).transform.position;
+            v.x = Mathf.Round(v.x);
+            v.y = Mathf.Round(v.y);
+            v.z = 0;
+            transform.GetChild(i).transform.position = v;
+        }
+    }
+}
