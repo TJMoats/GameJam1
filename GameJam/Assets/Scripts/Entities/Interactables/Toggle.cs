1170d59b471e4e2bd6de56be270fce2d6c9cf4e3
TJ Moats <tj@moats.io>
2019-02-09 21:13:34 -0700

    restructuring interactables

---
+++ GameJam/Assets/Scripts/Entities/Interactables/Toggle.cs
@@ -0,0 +1,42 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle : InteractableComponent
+{
+    [SerializeField]
+    private bool canToggle = true;
+    private bool isActive = false;
+
+    [SerializeField]
+    protected GameObject toToggle;
+    protected virtual GameObject ToToggle
+    {
+        get => toToggle;
+    }
+
+    public override void ReceiveInteraction(InteractionComponent _target)
+    {
+        ToggleGo();
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (toToggle != null)
+        {
+            isActive = toToggle.activeSelf;
+        }
+    }
+
+    [Button, LabelText("Toggle")]
+    public void ToggleGo()
+    {
+        if (canToggle && toToggle != null)
+        {
+            isActive = !isActive;
+            toToggle.SetActive(isActive);
+        }
+    }
+}
